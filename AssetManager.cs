a19331752eb04ec5a08f9896103eb1c3bb49872a
Eugene Chiong <eugenecys@gmail.com>
2016-11-20 16:26:01 -0800

    Added Asset Manager to link assets that are lost during scene transfers

---
+++ AssetManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetManager : Singleton<AssetManager>
+{
+    public GameObject[] assetSources;
+
+    public void findAllAssets()
+    {
+        foreach(GameObject assetSource in assetSources)
+        {
+            int linkCount = 0;
+            Asset[] assets = assetSource.GetInterfacesInChildren<Asset>();
+            foreach(Asset asset in assets)
+            {
+                if (asset.Link())
+                {
+                    linkCount++;
+                }
+            }
+            Debug.Log(linkCount + " Assets linked in source: " + assetSource.name);       
+        }
+    }
+}
